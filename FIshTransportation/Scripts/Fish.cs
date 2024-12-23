b67515c3c0ff452aea7114556ff89a934916da21
DavletgaraevAyaz <143988822+DavletgaraevAyaz@users.noreply.github.com>
2024-12-23 08:40:58 +0300

    add fish class

---
+++ FIshTransportation/Scripts/Fish.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FIshTransportation.Scripts
+{
+    public class Fish
+    {
+        public string Type { get; set; }
+        public int MaxTemperature { get; set; }
+        public int MaxTime { get; set; }
+        public int MinTemperature { get; set; }
+        public int MinTime { get; set; }
+    }
+
+    public class TemperatureRecord
+    {
+        public string DateTime { get; set; }
+        public int Temperature { get; set; }
+    }
+
+    public class Report
+    {
+        public DateTime ViolationTime { get; set; }
+        public int RequiredTemperature { get; set; }
+        public int ActualTemperature { get; set; }
+        public int Deviation { get; set; }
+    }
+}
